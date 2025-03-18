1e364393f5424853be73905d2a760a99240ec81d
osnott <hi@osnott.xyz>
2025-03-18 23:06:43 +0000

    feat(plug): add lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -91,4 +91,22 @@ return {
     opts = {
     },
   },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "open LazyGit" }
+    }
+  }
 }
