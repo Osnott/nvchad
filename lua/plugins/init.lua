fa9f4d0e5f07a4849a4a5360063bda235220b64f
osnott <hi@osnott.xyz>
2025-03-18 09:02:36 +0000

    feat(lazy): add trouble.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,4 +23,13 @@ return {
   		},
   	},
   },
+
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    config = function()
+      dofile(vim.g.base46_cache .. "trouble")
+      require("trouble").setup()
+    end
+  }
 }
