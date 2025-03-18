113171f4aae1f3ec3bfd995789ef731f7a93a0e7
osnott <hi@osnott.xyz>
2025-03-18 10:16:42 +0000

    chore(format): format files

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,6 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both' -- to enable cursorline!
 o.shiftwidth = 2
 o.tabstop = 2
