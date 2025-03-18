712628532df157bbfd6f886017df25ca2603da67
osnott <hi@osnott.xyz>
2025-03-18 10:16:24 +0000

    fix(plug/treesitter): fix issues loading

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,11 +15,6 @@ return {
 
   {
   	"nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      dofile(vim.g.base46_cache .. "treesitter")
-      require("nvim-treesitter.configs").setup()
-    end,
   	opts = {
   		ensure_installed = {
   			"vim", "lua", "vimdoc",
