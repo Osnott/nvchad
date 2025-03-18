85fdf958f82d4dc766c0333877b124de1981fbe8
osnott <hi@osnott.xyz>
2025-03-18 23:07:17 +0000

    feat(binds): add `<C-Dir>` window binds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,8 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+
+map('n', "<C-Left>", "<C-w>h", { desc = "switch window left" })
+map('n', "<C-Right>", "<C-w>l", { desc = "switch window right" })
+map('n', "<C-Down>", "<C-w>j", { desc = "switch window down" })
+map('n', "<C-Up>", "<C-w>k", { desc = "switch window up" })
