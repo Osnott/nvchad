7a68a5d1f93e303f8a70fd8f860297be0377659c
osnott <hi@osnott.xyz>
2025-03-18 05:22:11 +0000

    fix(lsp): change nil to nixd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "nil" }
+local servers = { "html", "cssls", "nixd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
