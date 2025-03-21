482fe819d8e593998a500de72bd29775623ece3d
osnott <hi@osnott.xyz>
2025-03-21 04:07:09 +0000

    feat(lsp): add tinymyst

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "nixd", "clangd" }
+local servers = { "html", "cssls", "nixd", "clangd", "tinymist" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local ooo = function(client, bufnr)
