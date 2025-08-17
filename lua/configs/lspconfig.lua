a3cba9d69c57601754e77bf2795f3c8634855522
osnott <hi@osnott.xyz>
2025-08-17 05:28:47 +0000

    feat(lsp): add jdtls lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "nixd", "clangd", "tinymist" }
+local servers = { "html", "cssls", "nixd", "clangd", "tinymist", "jdtls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local ooo = function(client, bufnr)
