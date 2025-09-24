1d281630f2725f1638be118eb14b37b8a462e098
osnott <hi@osnott.xyz>
2025-09-24 10:48:29 -0700

    feat(plug/conform): update typst formatter

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,7 +3,7 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    typst = { "typstfmt" },
+    typst = { "prettypst" },
     nix = { "alejandra" },
   },
 
