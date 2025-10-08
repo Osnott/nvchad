d0e5a0d7fddff200442bfd0433282051b440ffaa
osnott <hi@osnott.xyz>
2025-10-07 20:10:20 -0700

    feat(lsp): add vtsls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "nixd", "clangd", "tinymist", "basedpyright" }
+local servers = { "html", "cssls", "nixd", "clangd", "tinymist", "basedpyright", "vtsls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local ooo = function(client, bufnr)
